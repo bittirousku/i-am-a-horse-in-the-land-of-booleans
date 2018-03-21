8b0184b233b7f50d14c5ff88b31a6fa8a5227c76
Henrik Vesterinen <henrikv@iki.fi>
2018-03-21 18:51:03 +0200

    lecture2: exercises
    Signed-off-by: Henrik Vesterinen <henrikv@iki.fi>

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,77 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+  "Own boolean function."
+  [x]
+  (if
+    (or (= x nil) (= x false))
+    false
+    true
+    )
+  )
 
-(defn abs [x]
-  ":(")
+(defn abs
+  "Return absolute value."
+  [x]
+  (if
+    (< x 0)
+    (- 0 x)
+    x
+    )
+  )
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  [divisor n]
+  (if
+    (= 0 (mod n divisor))
+    true
+    false
+    )
+  )
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+  [n]
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else           ""
+    )
+  )
 
-(defn teen? [age]
-  ":(")
+(defn teen?
+  "Return truthy if age at least 13 and at most 19."
+  [age]
+  (if
+    (<= 13 age 19)
+    true
+    false
+    )
+  )
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen?
+  [age]
+  (not (teen? age))
+  )
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate
+  [x]
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true
+    )
+  )
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year?
+  [year]
+  (cond
+    (divides? 400 year) true
+    (and (divides? 4 year) (not (divides? 100 year))) true
+    :else   false
+    )
+  )
 
 ; '_______'
